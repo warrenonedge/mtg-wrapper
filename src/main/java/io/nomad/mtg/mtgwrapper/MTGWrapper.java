276562cfc9960b1eaa13ee1f916ad514adcbcdbb
warrenonedge <warrenonedge@gmail.com>
2017-11-11 19:28:07 -0500

    initial commit

---
+++ src/main/java/io/nomad/mtg/mtgwrapper/MTGWrapper.java
@@ -0,0 +1,13 @@
+package io.nomad.mtg.mtgwrapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class MTGWrapper 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
